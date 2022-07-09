efb2f448ca2b9ca77295f65bc546378d41619497
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-09 14:36:26 -0300

    cap 2 vid 1 finalizado

---
+++ redis/manipula-lista.js
@@ -0,0 +1,27 @@
+const { adiciona } = require("./manipula-blocklist");
+const {promisify} = require('util')
+module.exports = (lista) => {
+
+  const setAsync = promisify(lista.set).bind(lista)
+  const existsAsync = promisify(lista.exists).bind(lista)
+  const getAsync = promisify(lista.get).bind(lista)
+  const delAsync = promisify(lista.del).bind(lista)
+
+  return {
+    async adiciona(chave, valor, dataExpiracao) {
+      await setAsync(chave, valor)
+      lista.expireAt(chave, dataExpiracao)
+    },
+    async buscaValor(chave) {
+      return getAsync(chave)
+    },
+    async contemChave(chave) {
+      const resultado = await existsAsync(chave)
+      return resultado === 1
+    },
+
+    async deleta(chave) {
+      await delAsync(chave)
+    }
+  };
+};
