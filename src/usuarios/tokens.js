ef6ac7394f252ef81a36a05eaa98ef70729dfc1f
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-09 16:43:45 -0300

    cap 3 vid 2 finalizado

--- src/usuarios/tokens.js
+++ src/usuarios/tokens.js
@@ -2,14 +2,31 @@ const allowlistRefreshToken = require("../../redis/allowlist-refresh-token");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const moment = require("moment");
+const blocklistAccessToken = require("../../redis/blocklist-access-token");
+const { InvalidArgumentError } = require("../erros");
 
 function criaTokenJWT(id, [tempoQuantidade, tempoUnidade]) {
   const payload = {
     id,
   };
 
-  const token = jwt.sign(payload, process.env.CHAVE_JWT, { expiresIn: tempoQuantidade + tempoUnidade });
+  const token = jwt.sign(payload, process.env.CHAVE_JWT, {
+    expiresIn: tempoQuantidade + tempoUnidade,
+  });
   return token;
+}
+
+async function verificaTokenJWT(token, nome, blocklist) {
+  await verificaTokenNaBlocklist(token, nome, blocklist);
+  const { id } = jwt.verify(token, process.env.CHAVE_JWT);
+  return id;
+}
+
+async function verificaTokenNaBlocklist(token, nome, blocklist) {
+  const tokenNaBlocklist = await blocklist.contemToken(token);
+  if (tokenNaBlocklist) {
+    throw new jwt.JsonWebTokenError(`${nome} inválido por logout!`);
+  }
 }
 
 async function criaTokenOpaco(id, [tempoQuantidade, tempoUnidade], allowlist) {
@@ -19,18 +36,46 @@ async function criaTokenOpaco(id, [tempoQuantidade, tempoUnidade], allowlist) {
   return tokenOpaco;
 }
 
+async function verificaTokenOpaco(token, nome, allowlist) {
+  verificaTokenEnviado(token, nome);
+  const id = await allowlist.buscaValor(token);
+  verificaTokenValido(id, nome);
+  return id;
+}
+
+function verificaTokenValido(id, nome) {
+  if (!id) {
+    throw new InvalidArgumentError(`${nome} inválido!`);
+  }
+}
+
+function verificaTokenEnviado(token, nome) {
+  if (!token) {
+    throw new InvalidArgumentError(`${nome} não enviado!`);
+  }
+}
+
 module.exports = {
   access: {
-    expiracao: [15, 'm'],
+    nome: 'access token',
+    lista: blocklistAccessToken,
+    expiracao: [15, "m"],
     cria(id) {
-      return criaTokenJWT(id, this.expiracao)
+      return criaTokenJWT(id, this.expiracao);
+    },
+    verifica(token) {
+      return verificaTokenJWT(token, this.nome,  this.lista);
     },
   },
   refresh: {
+    nome: "refresh token",
     lista: allowlistRefreshToken,
-    expiracao: [5, 'd'],
+    expiracao: [5, "d"],
     cria(id) {
-      return criaTokenOpaco(id, this.expiracao, this.lista)
+      return criaTokenOpaco(id, this.expiracao, this.lista);
+    },
+    verifica(token) {
+      return verificaTokenOpaco(token, this.nome, this.lista);
     },
   },
 };
