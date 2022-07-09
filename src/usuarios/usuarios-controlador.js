b750d1241eed2728342d380c28d7565ee0f0005b
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-09 16:20:11 -0300

    cap 3 vid 1 finalizado

--- src/usuarios/usuarios-controlador.js
+++ src/usuarios/usuarios-controlador.js
@@ -1,26 +1,9 @@
 const Usuario = require('./usuarios-modelo');
 const { InvalidArgumentError } = require('../erros');
 
-const jwt = require('jsonwebtoken');
 const blocklist = require('../../redis/blocklist-access-token');
-const allowlistRefreshToken = require('../../redis/allowlist-refresh-token')
-const crypto = require('crypto')
-const moment = require('moment')
-function criaTokenJWT(usuario) {
-  const payload = {
-    id: usuario.id,
-  };
+const tokens = require('./tokens')
 
-  const token = jwt.sign(payload, process.env.CHAVE_JWT, { expiresIn: '15m' });
-  return token;
-}
-
-async function criaTokenOpaco(usuario) {
-  const tokenOpaco = crypto.randomBytes(24).toString('hex')
-  const dataExpiracao = moment().add(5, 'd').unix();
-  await allowlistRefreshToken.adiciona(tokenOpaco, usuario.id, dataExpiracao)
-  return tokenOpaco
-}
 
 module.exports = {
   async adiciona(req, res) {
@@ -45,8 +28,8 @@ module.exports = {
 
   async login(req, res) {
     try {
-      const accessToken = criaTokenJWT(req.user);
-      const refreshToken = await criaTokenOpaco(req.user)
+      const accessToken = tokens.access.cria(req.user.id);
+      const refreshToken = await tokens.refresh.cria(req.user.id)
       res.set('Authorization', accessToken);
       res.status(200).json({refreshToken});
     } catch (erro) {
